32848b4c884d30bca9024e1e4a008b573654c788
junranChen <617316241@qq.com>
2018-05-17 20:44:02 +0800

    循环赛日程表测试

--- src/test/test.java
+++ src/test/test.java
@@ -6,171 +6,77 @@ package test;
  */
 import java.util.Scanner;
 
+/**
+ * 循环赛日程表：有n = 2^k个运动员要进行网球循环赛
+ * 赛程表满足：
+ * 每个选手必须与其他n-1个选手各赛一次
+ * 每个选手一天只能参赛一次
+ * 循环赛在n-1天内结束
+ * 
+ * 解题思路：
+ * 将比赛日程表设计成一个n行和n-1列的表，第i行，第j列分别填入第i个选手在第j天所遇到的选手
+ * 栗子：
+ * 4个选手
+ * ---------
+ * |1|2|3|4|
+ * ---------
+ * |2|1|4|3|
+ * ---------
+ * |3|4|1|2|
+ * ---------
+ * |4|3|2|1|
+ * ---------
+ * 分治思想：将所有区域看成四块，区域1：(0,i) 区域2：(0,r+i) 区域3：(r,i) 区域4：(r,r+i)
+ *  递归执行的是区域1拷贝到区域4，区域2拷贝到区域3
+ * ---------
+ * | 1 | 2 |
+ * ---------
+ * | 3 | 4 |
+ * ---------
+ *  * @author 焦含寒
+ *
+ */
 public class test {
-    /**
-     * 
-     * @param n 图像灰度数组的大小
-     * @param p 图像灰度数组
-     * @param s s[i]表示从0到i压缩为一共占多少存储空间
-     * @param l l代表length
-     * @param b b代表bits
-     */
-    public void Compress(int n, int[] p, int[] s, int[] l, int[] b) {
-        int Lmax = 256;
-        int header = 11;
-        s[0] = 0;
-        for (int i = 1; i <= n; i++) {
-            b[i] = length(p[i]);// 计算像素点p需要的存储位数
-            int bmax = b[i];
-            s[i] = s[i - 1] + bmax;
-            l[i] = 1;
-            for (int j = 2; j <= i && j <= Lmax; j++) {
-                if (bmax < b[i - j + 1]) {
-                    bmax = b[i - j + 1];
-                }
-                if (s[i] > s[i - j] + j * bmax) {
-                    s[i] = s[i - j] + j * bmax;
-                    l[i] = j;
-                }
-            }
-            s[i] += header;
-        }
-    }
-
-    /*
-     * 计算整数的二进制的长度 return 长度；
-     */
-    public int length(int i) {
-        int k = 1;
-        i = i / 2;
-        while (i > 0) {
-            k++;
-            i = i / 2;
-        }
-        return k;
-    }
-
-    /*
-     * 构造最优解函数1
-     */
-    public int Traceback(int n, int i, int[] s, int[] l) {
-        if (n == 0)
-            return i;
-        i = Traceback(n - l[n], i, s, l);
-        s[i++] = n - l[n];// 重新为s[]数组赋值，用来存储分段位置
-        return i;
-    }
-
-    /*
-     * 构造最优解函数2
-     */
-    public void Output(int[] s, int[] l, int[] b, int n) {
-        // 在输出s[n]存储位数后，s[]数组则被重新赋值，用来存储分段的位置
-        System.out.println("图像压缩后的最小空间为： " + s[n]);
-        int m = 0;
-        m = Traceback(n, m, s, l);
-        s[m] = n;
-        System.out.println("将原灰度序列分成  " + m + " 段序列段");
-        for (int j = 1; j <= m; j++) {
-            l[j] = l[s[j]];
-            b[j] = Maxb(s, l, b, j);
-        }
-        for (int j = 1; j <= m; j++) {
-            System.out.println("段长度：" + l[j] + ",所需存储位数：" + b[j]);
-        }
-    }
-
-    /*
-     * 计算第j段中bmax
-     */
-    public int Maxb(int[] s, int[] l, int[] b, int j) {
-        int bmax = 0;
-        if (j == 1) {
-            bmax = b[1];
-            for (int i = 2; i <= s[j]; i++) {
-                if (bmax < b[i])
-                    bmax = b[i];
-            }
-        } else {
-            bmax = b[s[j - 1] + 1];
-            for (int i = s[j - 1] + 2; i <= s[j]; i++) {
-                if (bmax < b[i]) {
-                    bmax = b[i];
-                }
+	
+    public static int[][] table(int k){
+    	//移位运算，value=1，向左移动k位
+        int n = 1<<k;
+        int[][] a = new int[n][n];
+        //构造赛程表第一行数据
+        for(int i = 0; i<n;i++)
+            a[0][i] = i+1;
+        //采用分治算法，构造整个赛程表
+        for(int r = 1;r<n;r<<=1){
+            for(int i =0;i<n;i += 2*r){
+                copy(a,r,r+i,0,i,r);
+                copy(a,r,i,0,r+i,r);
             }
         }
-        return bmax;
+        return a;
     }
-    /**
-     * 图片压缩
-     */
-    public static void image_compressions(Scanner sc)
-            throws NumberFormatException {
-
-        String c = sc.next();
-        String[] o = c.split(",");
-        int[] p = new int[o.length + 1]; // 图像灰度数组 下标从1开始计数
-        for (int i = 1; i < p.length; i++) {
-            p[i] = Integer.parseInt(o[i - 1]);
+    
+    private static void copy(int[][] a, int tox, int toy, 
+            int fromx, int fromy, int r){
+        for(int i =0;i<r;i++){
+            for(int j = 0;j<r;j++){
+                a[tox+i][toy+j] = a[fromx+i][fromy+j];
+            }
         }
-
-        int N = p.length;
-        System.out.println();
-        // int N=7;
-        // int[] p={0,10,12,15,255,1,2};//图像灰度数组 下标从1开始计数
-        int[] s = new int[N];
-        int[] l = new int[N];
-        int[] b = new int[N];
-        System.out.println("图像的灰度值序列为：");
-        for (int i = 1; i < N; i++) {
-            System.out.print(p[i] + " ");
-        }
-        System.out.println();
-
-        test ic = new test();
-        ic.Compress(N - 1, p, s, l, b);
-        ic.Output(s, l, b, N - 1);
+        
     }
-
+        
+    
+    
     public static void main(String[] args) {
-        boolean flag = true;
-        boolean bflag = true;
-        Scanner sc = new Scanner(System.in);
-        a: while (flag) {
-            System.out.println("请输入图像的灰度序列(整数，以逗号为分隔符)：");
-            try {
-                image_compressions(sc);
-            } catch (Exception e) {
-                System.out.println("输入不符合规范，请重新输入！");
-                System.out
-                        .println("---------------------------------------------------------------------------------------");
-                System.out.println();
-                continue;
+
+        int[][] a = table(2);
+        for(int i=0;i<a.length;i++){
+            for(int j = 0;j<a[0].length;j++){
+                System.out.print(a[i][j] + "ss ");
             }
             System.out.println();
-            System.out
-                    .println("---------------------------------------------------------------------------------------");
-            System.out.println();
-            b: while (bflag) {
-                System.out.println("选择接下来的操作： 1--继续   2--退出");
-                int t = sc.nextInt();
-                switch (t) {
-                case 1:
-                    System.out
-                            .println("---------------------------------------------------------------------------------------");
-                    System.out.println();
-                    break b;
-                case 2:
-                    System.out.println("已退出！");
-                    break a;
-                default:
-                    System.out.println("没有可执行的操作，请重新输入！");
-                    System.out
-                            .println("---------------------------------------------------------------------------------------");
-                    System.out.println();
-                    break;
-                }
-            }
         }
+
     }
+
 }
