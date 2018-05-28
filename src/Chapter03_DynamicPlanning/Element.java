db1d4d18a8cef483d227d5f0f164afb7f8b9a022
junranChen <617316241@qq.com>
2018-05-28 14:45:59 +0800

    作业类

--- src/Chapter03_DynamicPlanning/Element.java
+++ src/Chapter03_DynamicPlanning/Element.java
@@ -2,11 +2,11 @@ package Chapter03_DynamicPlanning;
 
 public class Element implements Comparable{
 
-	int key;
-	int index;
-	boolean job;
+	private int key;//执行时间
+	private int index;//作业序号
+	private boolean job;//1代表第一个机器，0代表第二个机器
 	
-	Element(int kk,int ii, boolean jj){
+	public Element(int kk,int ii, boolean jj){
 		
 		key = kk;
 		index = ii;
@@ -16,6 +16,7 @@ public class Element implements Comparable{
 	public int compareTo(Object x) {
 		
 		int xkey = ((Element)x).key;
+		
 		if(key<xkey)
 			return -1;
 		
@@ -25,4 +26,22 @@ public class Element implements Comparable{
 		return 1;
 	}
 
+	public int getKey() {
+		return key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	public int getIndex() {
+		return index;
+	}
+	public void setIndex(int index) {
+		this.index = index;
+	}
+	public boolean isJob() {
+		return job;
+	}
+	public void setJob(boolean job) {
+		this.job = job;
+	}
 }
